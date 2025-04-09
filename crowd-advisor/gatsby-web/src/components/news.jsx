8035c2a52b4f5065d00c5d978a1ee0d59b31fba1
Mohammed Chauhan <crimsonavern@gmail.com>
2025-04-09 18:55:15 -0500

    Tweets/news responsive breakpoint columns

---
+++ crowd-advisor/gatsby-web/src/components/news.jsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import {Row, Col, Card, Button} from "react-bootstrap";
+import "../styles/masonry.css"
+import Masonry from 'react-masonry-css'
+
+
+export default function News(props) {
+
+  return (
+    <Row>
+      <Col>
+          <Masonry
+              breakpointCols = {{
+                default: 3,
+                1000: 2,
+                700: 1
+              }}
+              className="my-masonry-grid"
+              columnClassName="my-masonry-grid_column">
+              {props.news && props.news.map(newsArticle => newsArticle &&
+                  <Card key={newsArticle.id} style={{ width: '18rem' }}>
+                      <Card.Img variant="top" src={newsArticle.image} />
+                      <Card.Body>
+                          <Card.Title>{newsArticle.headline}</Card.Title>
+                          <Card.Text>
+                              {newsArticle.summary}
+                          </Card.Text>
+                          <a href={newsArticle.url}><Button variant="primary">From {newsArticle.source}</Button></a>
+                      </Card.Body>
+                  </Card>
+                  )}
+              </Masonry>
+      </Col>
+      <hr/>
+    </Row>
+
+  )
+
+}
